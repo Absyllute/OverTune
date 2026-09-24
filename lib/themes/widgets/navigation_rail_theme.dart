59d628e14fc1bf6728e29fdbf40d56d2e6545732
Λbsyllute <dev@absyllute.com>
2026-09-24 16:52:29 +0200

    theme: finalised custom navigation rail

--- lib/themes/widgets/navigation_rail_theme.dart
+++ lib/themes/widgets/navigation_rail_theme.dart
@@ -4,11 +4,21 @@ import 'package:overtune/global/current_theme.dart';
 final cNavigationRailTheme = NavigationRailThemeData(
   backgroundColor: CurrentTheme.theme.onBackground,
   indicatorColor: CurrentTheme.theme.primary,
+
   selectedIconTheme: IconThemeData(
-    color: CurrentTheme.theme.onBackground
+    color: CurrentTheme.theme.defaultTypography
   ),
 
   selectedLabelTextStyle: TextStyle(
+    color: CurrentTheme.theme.defaultTypography,
+    fontWeight: .bold
+  ),
+
+  unselectedIconTheme: IconThemeData (
+    color: CurrentTheme.theme.defaultTypography,
+  ),
+
+  unselectedLabelTextStyle: TextStyle(
     color: CurrentTheme.theme.defaultTypography
   )
 );
