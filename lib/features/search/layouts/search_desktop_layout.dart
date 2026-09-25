5e0ba1abf71bdf4f792cbec76a2d4fb7c6af9647
Λbsyllute <dev@absyllute.com>
2026-09-25 15:03:10 +0200

    widget: added Typography class and search bar widget

--- lib/features/search/layouts/search_desktop_layout.dart
+++ lib/features/search/layouts/search_desktop_layout.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../../../themes/typography.dart';
+
 class SearchDesktopLayout extends StatelessWidget {
   const SearchDesktopLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hi PC ppl"),
+    return Padding(
+      padding: .all(18.0),
+      child: Column(
+        children: [
+          SearchBar(
+            hintText: "Search for a song...",
+            hintStyle: .all(OTTypography.placeholder),
+
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/themes/typography.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:overtune/global/current_theme.dart';
+
+class OTTypography {
+  static final TextStyle placeholder = TextStyle(
+    color: CurrentTheme.theme.defaultTypography
+  );
+}
